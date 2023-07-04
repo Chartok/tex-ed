7253d6a6cf94ccb1b20ff05cddb037c865d6ad7e
Chartok <m.bhimjee@outlook.com>
2023-07-04 15:26:17 -0500

    working through webpack

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,9 +1,54 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+// const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+// const { CacheFirst } = require('workbox-strategies');
+// const { registerRoute } = require('workbox-routing');
+// const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+// const { ExpirationPlugin } = require('workbox-expiration');
+// const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+
+// precacheAndRoute(self.__WB_MANIFEST);
+
+// const pageCache = new CacheFirst({
+//   cacheName: 'page-cache',
+//   plugins: [
+//     new CacheableResponsePlugin({
+//       statuses: [0, 200],
+//     }),
+//     new ExpirationPlugin({
+//       maxAgeSeconds: 30 * 24 * 60 * 60,
+//     }),
+//   ],
+// });
+
+// warmStrategyCache({
+//   urls: ['/index.html', '/'],
+//   strategy: pageCache,
+// });
+
+// registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// // Define assetCache strategy
+// const assetCache = new CacheFirst({
+//   cacheName: 'asset-cache',
+//   plugins: [
+//     new CacheableResponsePlugin({
+//       statuses: [0, 200],
+//     }),
+//     new ExpirationPlugin({
+//       maxAgeSeconds: 30 * 24 * 60 * 60,
+//       maxEntries: 50,
+//     }),
+//   ],
+// })
+
+// // Cache requests like images, fonts, etc. using a Cache First strategy
+// registerRoute(({ request }) => request.destination !== 'document', assetCache);
+
+import { offlineFallback, warmStrategyCache } from 'workbox-recipes';
+import { CacheFirst } from 'workbox-strategies';
+import { registerRoute } from 'workbox-routing';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -38,7 +83,7 @@ const assetCache = new CacheFirst({
       maxEntries: 50,
     }),
   ],
-})
+});
 
 // Cache requests like images, fonts, etc. using a Cache First strategy
 registerRoute(({ request }) => request.destination !== 'document', assetCache);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,15 +1,79 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const BabelLoaderExcludeNodeModulesExcept = require('babel-loader-exclude-node-modules-except');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const WebpackPwaManifest = require('webpack-pwa-manifest');
+// const path = require('path');
+// const { InjectManifest } = require('workbox-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const BabelLoaderExcludeNodeModulesExcept = require('babel-loader-exclude-node-modules-except');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// // TODO: Add and configure workbox plugins for a service worker and manifest file.
+// // TODO: Add CSS loaders and babel to webpack.
 
-module.exports = () => {
+// module.exports = () => {
+//   return {
+//     mode: 'development',
+//     entry: {
+//       main: './src/js/index.js',
+//       install: './src/js/install.js'
+//     },
+//     output: {
+//       filename: '[name].bundle.js',
+//       path: path.resolve(__dirname, 'dist'),
+//     },
+//     plugins: [
+//       new CleanWebpackPlugin(),
+//       new HtmlWebpackPlugin({ template: './src/index.html' }),
+//       new WebpackPwaManifest({ /* manifwebpacest config */
+//         name: 'Just Another Text Editor',
+//         short_name: 'JATE',
+//         description: 'A simple text editor that works offline.',
+//         background_color: '#142C3B',
+//         crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+//         icons: [
+//           {
+//             src: path.resolve('src/img/icon.png'),
+//             sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+//             destination: path.join('assets', 'icons'),
+//           },
+          
+//         ],
+
+//           }),
+//       new InjectManifest({ swSrc: './src-sw.js', swDest: 'sw.js' }),
+//       new MiniCssExtractPlugin(),
+//     ],
+
+//     module: {
+//       rules: [
+//         {
+//           test: /\.js$/,
+//           exclude: BabelLoaderExcludeNodeModulesExcept(['idb']),
+//           use: {
+//             loader: 'babel-loader',
+//             options: { presets: ['@babel/preset-env'] },
+//           },
+//         },
+//         {
+//           test: /\.css$/,
+//           use: [MiniCssExtractPlugin.loader, 'css-loader'],
+//         },
+//       ]
+//     },
+//   };
+
+// };
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackPwaManifest from 'webpack-pwa-manifest';
+import path from 'path';
+import { InjectManifest } from 'workbox-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import BabelLoaderExcludeNodeModulesExcept from 'babel-loader-exclude-node-modules-except';
+
+const __dirname = path.resolve();
+
+export default () => {
   return {
     mode: 'development',
     entry: {
@@ -23,7 +87,8 @@ module.exports = () => {
     plugins: [
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({ template: './src/index.html' }),
-      new WebpackPwaManifest({ /* manifwebpacest config */
+      new WebpackPwaManifest({
+        /* manifest config */
         name: 'Just Another Text Editor',
         short_name: 'JATE',
         description: 'A simple text editor that works offline.',
@@ -31,18 +96,15 @@ module.exports = () => {
         crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
         icons: [
           {
-            src: path.resolve('src/img/icon.png'),
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
             destination: path.join('assets', 'icons'),
           },
-          
         ],
-
-          }),
+      }),
       new InjectManifest({ swSrc: './src-sw.js', swDest: 'sw.js' }),
       new MiniCssExtractPlugin(),
     ],
-
     module: {
       rules: [
         {
@@ -57,8 +119,7 @@ module.exports = () => {
           test: /\.css$/,
           use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
-      ]
+      ],
     },
   };
-
 };
