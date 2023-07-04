953d6baf97b2e0a0b67e22ceeb1029c8a5a6138b
Chartok <m.bhimjee@outlook.com>
2023-07-03 21:00:39 -0500

    server.js;routes

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,7 +1,6 @@
 import path from 'path';
 
   export default function(app) {
-
     app.get('/', (req, res) =>
       res.sendFile(path.join(__dirname, '../client/dist/index.html'))
     );
--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
-import express, { static, urlencoded, json } from 'express';
+// import express, { static, urlencoded, json } from 'express';
+import express from 'express';
+import path from 'path';
 express.static(root, [options]);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(static('../client/dist'));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
-require('./routes/htmlRoutes')(app);
+import htmlRoutes from './routes/htmlRoutes';
+htmlRoutes(app);
+
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
