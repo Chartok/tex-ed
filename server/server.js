7d78e5bce66b6157e22c97259a56fda8688629c9
Chartok <m.bhimjee@outlook.com>
2023-07-04 14:10:53 -0500

    fixing routes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 // import express, { static, urlencoded, json } from 'express';
 import express from 'express';
+import { urlencoded, json } from 'express';
 import path from 'path';
-express.static(root, [options]);
+import htmlRoutes from './routes/htmlRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,8 +11,8 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
-import htmlRoutes from './routes/htmlRoutes';
 htmlRoutes(app);
 
-
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+app.listen(PORT, () => { 
+    console.log(`Now listening on port: ${PORT}`);
+});
