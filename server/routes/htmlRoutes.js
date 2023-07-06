c4dbaed3ca71be1a1f217b508b08079fcc5b1b45
Chartok <m.bhimjee@outlook.com>
2023-07-06 17:07:47 -0500

    formatting&try/catch{async/await}

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,8 +1,13 @@
 const path = require('path');
 
-module.exports = function(app) {
-  app.get('/', async (req, res) => {
-    console.log('GET request received at /');
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
+module.exports = function (app) {
+  try {
+    app.get('/', async (req, res) => {
+      console.log('GET request received at /');
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  } catch (error) {
+    console.error('There was an error in the request handler');
+    throw error;
+  }
 };
