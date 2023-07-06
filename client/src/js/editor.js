a1d83a37bafbf0f2800da24e666c072247532894
Chartok <m.bhimjee@outlook.com>
2023-07-06 17:24:45 -0500

    finished editor&index.js

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,42 +1,52 @@
-// Import methods to save and get data from the indexedDB database in './database.js'
-// import { getDb, putDb } from './database.js';
-// import { header } from './header.js';
+const CodeMirror = require('codemirror');
+const { getDb, putDb } = require('./database.js');
+const { header } = require('./header.js');
 
-// export default class {
-//   constructor() {
-//     const localData = localStorage.getItem('content');
+class Editor {
+  constructor() {
+    const localData = localStorage.getItem('content');
 
-//     // check if CodeMirror is loaded
-//     if (typeof CodeMirror === 'undefined') {
-//       throw new Error('CodeMirror is not loaded');
-//     }
+    // check if CodeMirror is loaded
+    if (typeof CodeMirror === 'undefined') {
+      throw new Error('CodeMirror is not loaded');
+    }
 
-//     this.editor = CodeMirror(document.querySelector('#main'), {
-//       value: '',
-//       mode: 'javascript',
-//       theme: 'monokai',
-//       lineNumbers: true,
-//       lineWrapping: true,
-//       autofocus: true,
-//       indentUnit: 2,
-//       tabSize: 2,
-//     });
+    this.editor = CodeMirror(document.querySelector('#main'), {
+      value: '',
+      mode: 'javascript',
+      theme: 'monokai',
+      lineNumbers: true,
+      lineWrapping: true,
+      indentUnit: 2,
+      tabSize: 2
+    });
 
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
-//     getDb().then((data) => {
-//       console.info('Loaded data from IndexedDB, injecting into editor');
-//       this.editor.setValue(data || localData || header);
-//     });
+    (async () => {
+    try {
+      const data = await getDb();
+      console.info('Loaded data from IndexedDB, injecting into editor');
+      this.editor.setValue(data || localData || header);
+    } catch (error) {
+      console.error('There was an error loading data from IndexedDB');
+      this.editor.setValue(localData || header);
+    }
+    })();
 
-//     this.editor.on('change', () => {
-//       localStorage.setItem('content', this.editor.getValue());
-//     });
+    this.editor.on('change', () => {
+      localStorage.setItem('content', this.editor.getValue());
+    });
 
-//     // Save the content of the editor when the editor itself is loses focus
-//     this.editor.on('blur', () => {
-//       console.log('The editor has lost focus');
-//       putDb(localStorage.getItem('content'));
-//     });
-//   }
-// }
+    // Save the content of the editor when the editor itself is loses focus
+    this.editor.on('blur', async () => {
+      console.log('Lost focus; saving content to IndexedDB...');
+      try {
+        await putDb(localStorage.getItem('content'));
+      } catch (error) {
+        console.error('There was an error saving data to IndexedDB');
+        throw error;
+      }
+    });
+  }
+}
+
+module.exports = Editor;
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,33 +1,58 @@
-// import { Workbox } from 'workbox-window';
-// import Editor from './editor.js';
-// import './database.js';
-// import '../css/style.css';
+const { Workbox } = require('workbox-window');
+const Editor = require('./editor.js');
+require('./database.js');
+require('../css/style.css');
 
-// const main = document.querySelector('#main');
-// main.innerHTML = '';
+const main = document.querySelector('#main');
+main.innerHTML = '';
 
-// const loadSpinner = () => {
-//   const spinner = document.createElement('div');
-//   spinner.classList.add('spinner');
-//   spinner.innerHTML = `
-//   <div class="loading-container">
-//   <div class="loading-spinner" />
-//   </div>
-//   `;
-//   main.appendChild(spinner);
-// };
+const loadSpinner = () => {
+  const spinner = document.createElement('div');
+  spinner.classList.add('spinner');
+  spinner.innerHTML = `
+  <div class="loading-container">
+  <div class="loading-spinner" />
+  </div>
+  `;
+  main.appendChild(spinner);
+};
 
-// const editor = new Editor();
+const initializeEditor = async () => {
+  try {
+    const editor =  new Editor();
+    if (typeof editor === 'undefined') {
+      loadSpinner();
+    }
+  } catch (error) {
+    console.error('There was an error initializing the editor');
+    throw error;
+  }
+};
 
-// if (typeof editor === 'undefined') {
-//   loadSpinner();
-// }
+const registerServiceWorker = async () => {
+  try {
+    // Check if service workers are supported
+    if ('serviceWorker' in navigator) {
+      // register workbox service worker
+      const workboxSW = new Workbox('./src-sw.js');
+      await workboxSW.register();
+    } else {
+      console.error('Service workers are not supported in this browser.');
+    }
+  } catch (error) {
+    console.error('There was an error registering the service worker');
+    throw error;
+  }
+};
 
-// // Check if service workers are supported
-// if ('serviceWorker' in navigator) {
-//   // register workbox service worker
-//   const workboxSW = new Workbox('./src-sw.js');
-//   workboxSW.register();
-// } else {
-//   console.error('Service workers are not supported in this browser.');
-// }
+const initializeApp = async () => {
+  try {
+    await initializeEditor();
+    await registerServiceWorker();
+  } catch (error) {
+    console.error('There was an error initializing the app');
+    throw error;
+  }
+};
+
+initializeApp();
