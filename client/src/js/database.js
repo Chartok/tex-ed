25bbd2b5f05295987485062775fa8ba9de020117
Chartok <m.bhimjee@outlook.com>
2023-07-06 16:44:22 -0500

    database.js done

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,34 +1,52 @@
-// import { openDB } from 'idb';
+const { openDB } = require('idb');
 
-// const initdb = async () =>
-//   openDB('jate', 1, {
-//     upgrade(db) {
-//       if (db.objectStoreNames.contains('jate')) {
-//         console.log('jate database already exists');
-//         return;
-//       }
-//       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-//       console.log('jate database created');
-//     },
-//   });
+const initdb = async () => {
+  try {
+  const db = await openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
 
-// // logic to a method that accepts some content and adds it to the database
-// export const putDb = async (content) => {
-//   const db = await initdb();
-//   const tx = db.transaction('jate', 'readwrite');
-//   const store = tx.objectStore('jate');
-//   await store.put(content); // *await* the put() method
-//   await tx.done;
-// } ;
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('Jate DB created');
+    },
+  });
+  return db;
+  } catch (error) {
+    console.error("Error initializing database: ");
+    throw error;
+  }
+};
 
-// // logic for a method that gets all the content from the database
-// export const getDb = async () => {
-//   const db = await initdb();
-//   const tx = db.transaction('jate', 'readonly');
-//   const store = tx.objectStore('jate');
-//   const content = await store.getAll();
-//   await tx.done;
-//   return content[content.length - 1]?.content; // 
-// };
+exports.putDb = async (content) => {
+  try {
+    const db = await initdb();
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    await store.put(content); // *await* the put() method
+    await tx.done;
+  } catch (error) {
+    console.error("Error writing to database: ");
+    throw error;
+  }
+};
 
-// initdb();
+exports.getDb = async () => {
+  try {
+    const db = await initdb();
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const content = await store.getAll();
+    await tx.done;
+
+    // Return the last item in the array of content if it exists, otherwise return undefined
+    return content[content.length - 1]?.content;
+  } catch (error) {
+    console.error("Error reading from database: ");
+    throw error;
+  }
+};
+
+initdb();
