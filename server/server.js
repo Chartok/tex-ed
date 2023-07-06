2fc94c183ca86bd652a22d932cf03e4b6a29894e
Chartok <m.bhimjee@outlook.com>
2023-07-06 17:05:58 -0500

    server.js done

--- server/server.js
+++ server/server.js
@@ -1,3 +1,35 @@
+const express = require('express');
+const { urlencoded, json } = require('express');
+const { fileURLToPath } = require('url');
+const { dirname, join } = require('path');
+const htmlRoutes = require('./routes/htmlRoutes.js');
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const setupRoutes = async () => {
+    try {
+        app.use(express.static(join(__dirname, '../client/dist')));
+        app.use(urlencoded({ extended: true }));
+        app.use(json());
+
+        console.log('Setting up routes...');
+        await htmlRoutes(app);
+
+        app.listen(PORT, () => {
+            console.log(`Now listening on port: ${PORT}`);
+        });
+    } catch (error) {
+        console.error('There was an error setting up routes');
+        throw error;
+    }
+};
+
+setupRoutes();
+
 // import express from 'express';
 // import { urlencoded, json } from 'express';
 // import { fileURLToPath } from 'url';
