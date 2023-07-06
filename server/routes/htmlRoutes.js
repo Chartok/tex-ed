19640a187e9c1833e1bd05fe16c87ee089c3adf6
Chartok <m.bhimjee@outlook.com>
2023-07-06 16:45:01 -0500

    htmlRoutes done

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,12 +1,3 @@
-// import path from 'path';
-
-//   export default function(app) {
-//     app.get('/', (req, res) => {
-//     console.log('GET request received at /');
-//       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-//   });
-//   }
-
 const path = require('path');
 
 module.exports = function(app) {
